b3deb6f555ca722816c89e6b45e6f41b7271f7da
harigeeda <109517131+harigeeda@users.noreply.github.com>
2022-09-15 19:11:53 +0530

    a=input() b=len(a) z=0 for i in range(0,b):     z+=int(a[i]) k=str(z) m=len(k) n=0 for j in range(0,m):     n+=int(k[j]) k1=str(n) m1=len(k1) n1=0 for hari in range(0,m1):     n1+=int(k1[hari]) print(n1)

---
+++ Add_Digits.py
@@ -0,0 +1,16 @@
+a=input()
+b=len(a)
+z=0
+for i in range(0,b):
+    z+=int(a[i])
+k=str(z)
+m=len(k)
+n=0
+for j in range(0,m):
+    n+=int(k[j])
+k1=str(n)
+m1=len(k1)
+n1=0
+for hari in range(0,m1):
+    n1+=int(k1[hari])
+print(n1)
