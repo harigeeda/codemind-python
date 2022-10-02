0be65738c9d7727d9071020596cc49377fa22a61
harigeeda <109517131+harigeeda@users.noreply.github.com>
2022-10-02 21:48:23 +0530

    x=input() b=list(map(int,input().split())) a,x=map(int,input().split()) s=[] for i in b:     if i>=a and i<=x:         s.append(i) if (len(s))>0:     print(*(s)) else:     print("-1")

---
+++ elements_in_between_A_and_B.py
@@ -0,0 +1,11 @@
+x=input()
+b=list(map(int,input().split()))
+a,x=map(int,input().split())
+s=[]
+for i in b:
+    if i>=a and i<=x:
+        s.append(i)
+if (len(s))>0:
+    print(*(s))
+else:
+    print("-1")
